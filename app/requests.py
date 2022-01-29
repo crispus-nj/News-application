a3df29aaec0ab7d8e9a5b9061a482b510cc6bd87
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-29 18:10:19 +0300

    added api_key and two base_urls in the requests file. wrote a function to get data from tech_crunch api and displayed the data to screen

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,14 @@
 from app import app
 import requests
+from .config import Config
 
+Api_key = app.config['API_KEY']
+tech_base_url = app.config['TECH_CRUNCH_BASE_URL']
+business_base_url=app.config['BUSINESS_BASE_URL']
+
+def tech_news():
+    base_url = tech_base_url.format(Api_key)
+    tech_data = requests.get(base_url).json()
+    for tech in tech_data['articles']:
+        print(tech['author'])
+    return tech_data
--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from app import app
 from flask import render_template
-
+from .requests import tech_news
 @app.route('/')
 def homepage():
     '''
     homepage function runs everytime the application is runned without any routes attached
     '''
-    return render_template('index.html')
+    tech_link = tech_news()
+
+    return render_template('index.html', tech_link=tech_link)
