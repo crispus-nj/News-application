e03cfa1ded24ada97bfb7b939d4fa11d059aef99
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-30 07:37:32 +0300

    added a function tpo fetch all articles data to be displayed in the index.html as courousel

--- app/requests.py
+++ app/requests.py
@@ -2,10 +2,14 @@ from app import app
 import requests
 from .config import Config
 import os
+from .models import news_article
+
+News_article = news_article.News_Article
 
 Api_key = os.getenv('API_KEY')
 tech_base_url = app.config['TECH_CRUNCH_BASE_URL']
 business_base_url=app.config['BUSINESS_BASE_URL']
+all_article_base_url= app.config['ALL_ARTICLES_BASE_URL']
 
 def tech_news():
     base_url = tech_base_url.format(Api_key)
@@ -13,3 +17,12 @@ def tech_news():
     for tech in tech_data['articles']:
         print(tech['author'])
     return tech_data
+
+def all_articles_news():
+    '''
+    all_articles_news function displays all news from various sources without specification
+    '''
+    base_url = all_article_base_url.format(Api_key)
+    all_article_data = requests.get(base_url).json()
+    return all_article_data
+
