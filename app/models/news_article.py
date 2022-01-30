9cac2e1927c436bb36cbba58d357694374dd9d35
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-30 07:38:40 +0300

    added NewArticle class and created the constructor function with all the class objects

--- app/models/news_article.py
+++ app/models/news_article.py
@@ -1,2 +1,11 @@
 class News_Article:
-    pass
+    '''
+    News_Article class to define News_Article Objects
+    '''
+    def __init__(self, id, poster, url_link, description, published_date, content):
+        self.id = id
+        self.poster = poster
+        self.url_link = url_link
+        self.description = description
+        self.published_date = published_date
+        self.content = content
