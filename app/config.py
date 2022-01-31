a4b9278f3e085b3a58cd2b0d6de41a043e1f5620
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-31 09:53:48 +0300

    changed all articles url in the config.py file. added date to the index.html content. added flexbox to the published section

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     parent class of all other config sub class
     '''
-    ALL_ARTICLES_BASE_URL="https://newsapi.org/v2/everything?domains=wsj.com&apiKey={}"
+    ALL_ARTICLES_BASE_URL="https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={}"
     TECH_CRUNCH_BASE_URL = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={}"
     BUSINESS_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={}'
     TESLA_BASE_URL = "https://newsapi.org/v2/everything?q=tesla&from=2021-12-29&sortBy=publishedAt&apiKey={}"
--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
+from .requests import tech_news, all_articles_news, business_news, apple_news
 from app import app
 from flask import render_template
-from .requests import tech_news, all_articles_news, business_news, apple_news
 
 @app.route('/')
 def homepage():
     article_data = all_articles_news()
     business_data = business_news()
     apple_data = apple_news()
-    return render_template('index.html', article_data = article_data, business_data=business_data, apple_data=apple_data)
+    return  render_template('index.html', article_data = article_data, apple_data = apple_data, business_data = business_data)
 
 @app.route('/tech-crunch')
 def tech_crunch():
